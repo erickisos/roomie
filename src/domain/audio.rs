cf5007f2a5305348e67782f196c7e20f395adf47
Erick G. Islas-Osuna <erickisos653@gmail.com>
2023-02-19 23:43:59 -0600

    WIP: Add some extra audio functions

--- src/domain/audio.rs
+++ src/domain/audio.rs
@@ -1,13 +1,37 @@
-use cpal::{self, traits::HostTrait, Devices, DevicesError};
+use cpal::{
+    self,
+    traits::{DeviceTrait, HostTrait},
+    Device,
+};
 
-pub(crate) fn get_inputlist(
-) -> Result<std::iter::Filter<Devices, for<'r> fn(&'r cpal::Device) -> bool>, DevicesError> {
-    let host = cpal::default_host();
-    return host.input_devices();
+pub(crate) fn get_outputlist() -> Vec<String> {
+    return cpal::default_host()
+        .output_devices()
+        .unwrap()
+        .map(|device| device.name().unwrap())
+        .collect();
 }
 
-pub(crate) fn get_outputlist(
-) -> Result<std::iter::Filter<Devices, for<'r> fn(&'r cpal::Device) -> bool>, DevicesError> {
-    let host = cpal::default_host();
-    return host.output_devices();
+pub(crate) fn get_inputlist() -> Vec<String> {
+    return cpal::default_host()
+        .input_devices()
+        .unwrap()
+        .map(|device| device.name().unwrap())
+        .collect();
 }
+
+pub(crate) fn sweep(output_device: String) {
+    let device = find_device(&output_device);
+    play(&device)
+}
+
+fn find_device(device_name: &str) -> Device {
+    return cpal::default_host()
+        .devices()
+        .unwrap()
+        .into_iter()
+        .find(|device| device.name().unwrap() == device_name)
+        .unwrap();
+}
+
+fn play(device: &Device) {}
--- src/ports/widgets.rs
+++ src/ports/widgets.rs
@@ -1,17 +1,8 @@
-use cpal::{traits::DeviceTrait, Device, Devices, DevicesError};
-use gtk::{traits::BoxExt, Box, DropDown, Label, Orientation};
-use std::iter::Filter;
+use gtk::{traits::BoxExt, Align, Box, DropDown, Label, Orientation};
 
-fn build_dropdown(
-    devices_list: Result<Filter<Devices, fn(&Device) -> bool>, DevicesError>,
-) -> DropDown {
-    let device_names: Vec<String> = devices_list
-        .unwrap()
-        .map(|device| device.name().unwrap())
-        .collect();
-
+fn build_dropdown(devices_list: Vec<String>) -> DropDown {
     let dropdown = DropDown::from_strings(
-        device_names
+        devices_list
             .iter()
             .map(|name| name.as_str())
             .collect::<Vec<&str>>()
@@ -22,10 +13,7 @@ fn build_dropdown(
     return dropdown;
 }
 
-pub(crate) fn build_labeled_dropdown(
-    label: &str,
-    devices_list: Result<Filter<Devices, fn(&Device) -> bool>, DevicesError>,
-) -> Box {
+pub(crate) fn build_labeled_dropdown(label: &str, devices_list: Vec<String>) -> Box {
     let container_box = Box::builder()
         .orientation(Orientation::Horizontal)
         .spacing(10)
@@ -37,10 +25,7 @@ pub(crate) fn build_labeled_dropdown(
     container_box.append(&dropdown);
     return container_box;
 }
-fn build_audio_options(
-    inputs: Result<std::iter::Filter<Devices, for<'r> fn(&'r cpal::Device) -> bool>, DevicesError>,
-    outputs: Result<std::iter::Filter<Devices, for<'r> fn(&'r cpal::Device) -> bool>, DevicesError>,
-) -> Box {
+fn build_audio_options(inputs: Vec<String>, outputs: Vec<String>) -> Box {
     let horizontal_box = Box::builder()
         .orientation(Orientation::Horizontal)
         .spacing(10)
@@ -55,10 +40,7 @@ fn build_audio_options(
     return horizontal_box;
 }
 
-pub(crate) fn build_layout(
-    inputs: Result<std::iter::Filter<Devices, for<'r> fn(&'r cpal::Device) -> bool>, DevicesError>,
-    outputs: Result<std::iter::Filter<Devices, for<'r> fn(&'r cpal::Device) -> bool>, DevicesError>,
-) -> Box {
+pub(crate) fn build_layout(inputs: Vec<String>, outputs: Vec<String>) -> Box {
     let main_box = Box::builder()
         .orientation(Orientation::Vertical)
         .spacing(10)
